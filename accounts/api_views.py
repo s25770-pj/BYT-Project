61b9028b7cb11853373de885fdf9fea79070dc40
s25770-pj <s25770@pjwstk.edu.pl>
2023-10-08 23:12:30 +0200

    Added registration for parent and child + endpoints

--- accounts/api_views.py
+++ accounts/api_views.py
@@ -1,30 +1,65 @@
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.shortcuts import render, redirect
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer
+from django.contrib.auth import logout
+from .models import Parent, Child
 
 
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_account_view(request):
+    if request.method == 'DELETE':
+        user = request.user
+        user.delete()
+        logout(request)
+
+        return Response({'message': 'Konto zostało usunięte'}, status=status.HTTP_204_NO_CONTENT)
+    return Response({'message': 'Nieprawidłowe żądanie'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')
-        else:
-            form = AuthenticationForm()
-        return render(request, 'registration/login.html', {'form': form})
-
-
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
             login(request, user)
-            return redirect('profile')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
+            return Response({'message:': 'Zalogowano pomyślnie'}, status=status.HTTP_200_OK)
+    return Response({'message': ' Błąd logowania'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['POST'])
+def register_parent_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            login(request, user)
+            return Response({'message': 'Zarejestrowano pomyślnie'}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def register_child_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            parent = request.user
+            username = request.data.get('username')
+            password = request.data.get('password')
+
+            child = Child(
+                parent=parent,
+                username=username,
+                password=password,
+            )
+            child.save()
+            return Response({'message': 'Konto dziecka stworzone pomyślnie'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'message': 'Nieprawidłowe dane'}, status=status.HTTP_400_BAD_REQUEST)
--- accounts/models.py
+++ accounts/models.py
@@ -1,31 +1,50 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, Permission, Group
 from django.utils.translation import gettext as _
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Permission, PermissionsMixin, Group
 
 
-class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
-    profile_picture = models.ImageField(blank=True, null=True)
+class Parent(AbstractUser, PermissionsMixin):
+    login = models.CharField(max_length=50, blank=False, null=False)
+    password = models.CharField(max_length=50, blank=False, null=False)
+    email = models.CharField(max_length=100, blank=False, null=False)
     is_active = models.BooleanField(default=True)
     is_blocked = models.BooleanField(default=False)
-    ROLE_CHOICES = (
-        ('user', 'Użytkownik'),
-        ('admin', 'Administrator'),
-        ('moderator', 'Moderator'),
-    )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
     groups = models.ManyToManyField(
         Group,
-        verbose_name=('groups'),
+        verbose_name=_('Grupy'),
         blank=True,
-        related_name='customuser_set',
-        related_query_name='user'
+        related_name='parent_set',
+        related_query_name='parent'
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        verbose_name=_('user permissions'),
+        verbose_name=_('Uprawnienia użytkownika'),
         blank=True,
-        related_name='customuser_set',
-        related_query_name='user',
+        related_name='parent_user_permissions',
+        related_query_name='user'
     )
+    role = models.CharField(max_length=20, default='Rodzic')
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = 'Rodzic'
+        verbose_name_plural = 'Rodzice'
+
+
+class Child(models.Model):
+    role = models.CharField(max_length=10, default='Dziecko')
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30, blank=True, null=True, unique=True)
+    password = models.CharField(max_length=50, blank=False, null=False)
+    profile_picture = models.ImageField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.username}"
+
+    class Meta:
+        verbose_name = 'Dziecko'
+        verbose_name_plural = 'Dzieci'
