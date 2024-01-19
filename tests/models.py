10550fcf2115907c8ddad86003f5971c7fc62d99
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-20 00:41:03 +0100

    fix

--- tests/models.py
+++ tests/models.py
@@ -13,6 +13,7 @@ class Exercise(models.Model):
     def get_time_left(cls, obj):
         return obj.time_left.total_seconds()
 
+
 class ClassRoom(models.Model):
     name = models.CharField(max_length=128, unique=True)
     code = models.CharField(max_length=32, unique=True)
