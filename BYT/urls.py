da8bf9a9b8c8bad642c100a6b9189a2a349423fa
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-20 00:12:36 +0100

    fix

--- BYT/urls.py
+++ BYT/urls.py
@@ -1,4 +1,6 @@
+from patterns import patterns
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path, include, re_path
 from django.conf import settings
 from drf_yasg.views import get_schema_view
@@ -19,7 +21,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path('swagger(?P<format>)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path('swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path('redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
@@ -31,3 +33,4 @@ urlpatterns += [
     ])),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
+
