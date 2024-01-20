e2cf6053f42a3704287531a335b4f42c12a26c48
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-20 23:44:08 +0100

    fix

--- accounts/api_urls.py
+++ accounts/api_urls.py
@@ -5,7 +5,7 @@ from .api_views import CustomLoginView, LogoutView, RegisterView, UserDataView
 app_name = 'api_accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('user-data/', UserDataView.as_view(), name='user-data'),
