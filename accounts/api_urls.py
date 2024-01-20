8607641879d07445f92a5d428b0d7661d280d76e
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-21 00:05:30 +0100

    fix

--- accounts/api_urls.py
+++ accounts/api_urls.py
@@ -1,11 +1,12 @@
 # from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 # from django.urls import path
 # from .api_views import CustomLoginView, LogoutView, RegisterView, UserDataView
+from django.contrib.auth.views import LoginView
 
 app_name = 'api_accounts'
 
 urlpatterns = [
-#     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginView.as_view(), name='login_view'),
 #     path('logout/', LogoutView.as_view(), name='logout'),
 #     path('register/', RegisterView.as_view(), name='register'),
 #     path('user-data/', UserDataView.as_view(), name='user-data'),
