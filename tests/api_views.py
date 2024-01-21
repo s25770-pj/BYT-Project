82fd12dce86ac4f7e943e22fa106d33c5f63f3b0
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-21 01:43:08 +0100

    fix

--- tests/api_views.py
+++ tests/api_views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Exercise
+from .models import Exercise, ClassRoom
 from .serializers import ExerciseSerializer, ClassRoomSerializer
 
 
@@ -16,3 +16,6 @@ class GetExercisesListApiView(generics.ListAPIView):
 class GetClassRoomsListApiView(generics.ListAPIView):
     serializer_class = ClassRoomSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return ClassRoom.objects.all()
