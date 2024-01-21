586cce81650d16988fe759b8800ea1c199f5f613
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-21 17:46:47 +0100

    fix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -29,7 +29,17 @@ class GetUserSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        return user
 
 
 class LogoutSerializer(serializers.ModelSerializer):
