5867a990b493495d3ac1995e412fa6c39e95f16f
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-26 17:58:46 +0100

    Added type field to user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+TypeChoices = [
+    ('uczeń', 'Student'),
+    ('nauczyciel', 'Teacher')
+]
+
 
 class UserEquipment(models.Model):
     pass
 
 
 class CustomUser(AbstractUser):
+    type = models.CharField(max_length=20, verbose_name='Typ', choices=TypeChoices, default=TypeChoices[0])
     equipment = models.ForeignKey(UserEquipment, null=True, blank=True, on_delete=models.CASCADE)
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-from tests.models import ClassRoom
-
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -19,45 +17,10 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['username', 'password']
-
-
-class GetUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = '__all__'
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ['username', 'email', 'password', 'first_name', 'last_name']
-
-    def create(self, validated_data):
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-        )
-        return user
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'type']
 
 
 class LogoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = '__all__'
-
-
-class ClassSerializer(serializers.ModelSerializer):
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        classroom = ClassRoom.objects.create(created_by=user, name=validated_data['name'])
-        classroom.users.set([user])
-        return {'response': 'Classroom successfully created', 'classroom': classroom}
-
-    class Meta:
-        model = ClassRoom
         fields = '__all__'
