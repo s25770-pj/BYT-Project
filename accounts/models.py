45765c3c409df49dd91e30f3c143de48fde7b7fb
s25770-pj <s25770@pjwstk.edu.pl>
2024-01-26 17:46:12 +0100

    added account typs

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+userChoices = [
+    ('nauczyciel', 'Teacher'),
+    ('uczeń', 'Student')
+]
+
 
 class UserEquipment(models.Model):
     pass
 
 
 class CustomUser(AbstractUser):
+    type = models.CharField(max_length=10, default="user", choices=userChoices)
     equipment = models.ForeignKey(UserEquipment, null=True, blank=True, on_delete=models.CASCADE)
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -17,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'type']
 
 
 class LogoutSerializer(serializers.ModelSerializer):
