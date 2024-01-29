89b353c9db01cfd931665454a6361e1e23c3f7c9
s25770-pj <s25770@pjatk.edu.pl>
2024-01-29 20:02:38 +0100

    Procfile change

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn BYT.wsgi
-release: python manage.py migrate && python manage.py loaddata tests_fixtures.json
+release: python manage.py migrate
--- accounts/api_views.py
+++ accounts/api_views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth import authenticate, login, logout
 
 from rest_framework.response import Response
 from rest_framework import generics, status
