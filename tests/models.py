520001afe7358b490d94fbd765a940bcbab9ec19
s25770-pj <s25770@pjatk.edu.pl>
2024-01-30 22:28:07 +0100

    modified user model

--- tests/models.py
+++ tests/models.py
@@ -10,6 +10,7 @@ class Exercise(ModelBase):
     name = models.CharField(max_length=128, unique=False)
     time = models.DurationField()
     difficulty = models.CharField(max_length=64, choices=C_DIFFICULTY_CHOICES)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name='created_exercise')
 
     @classmethod
     def get_time_left(cls, obj):
@@ -20,7 +21,8 @@ class ClassRoom(ModelBase):
     name = models.CharField(max_length=128, unique=True)
     code = models.CharField(max_length=16, unique=True)
     exercises = models.ManyToManyField(Exercise, blank=True)
-    members = models.ManyToManyField(CustomUser, blank=False, related_name='created_classrooms')
+    members = models.ManyToManyField(CustomUser, blank=False, related_name='members_classrooms')
+    created_by = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name='created_classrooms')
 
     def __str__(self):
         return self.name
--- utils/models.py
+++ utils/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-from accounts.models import CustomUser
-
 
 class ModelBase(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey( CustomUser, on_delete=models.PROTECT)
 
     class Meta:
         abstract = True
